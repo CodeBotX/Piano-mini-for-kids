abdb442a9bc964fad110e9708b8d9419b1e60549
MinhTien999 <minhtien161002@gmail.com>
2023-03-09 12:16:17 +0700

    Minh Tien Upload

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,24 @@
 #include<Arduino.h>
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include<BLEServer.h>
-#include <BLE2902.h>
-BLEServer* pServer = NULL;
-BLECharacteristic* pCharacteristic = NULL;
-BLEDescriptor *pDescr;
-BLE2902 *pBLE2902;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint32_t value = 0;
-#define SERVICE_UUID "dd1a54fe-d993-452e-99de-4159d4d4e17e"
-#define CHARACTERISTIC_UUID "c3ae4e02-a1ac-47ea-9da2-5fd3cf42ae41"
-
-class MyServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      deviceConnected = true;
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      deviceConnected = false;
-    }
-};
-
-
 
 TaskHandle_t Task1;
 TaskHandle_t Task2;
 
+int melody1[1000];
+int melody1Index=0;
 
+// bai hat huong dan mac dinh
 #define SoluongNote 42
 const int music[]={0,0,7,7,9,9,7,5,5,4,4,2,2,0,7,7,5,5,4,4,2,7,7,5,5,4,4,2,0,0,7,7,9,9,7,5,5,4,4,2,2,0};
-int melody1[1000], melodyIndex = 0;
-
+// khai bao chan nut
 const int BUTTON_PIN[]={21,22,23,25,26,27,32,33,35,34,36,39};
-
+// khai bao chan loa
 #define BUZZER_PIN 15
 
 const float NOTE[]={261.6256,277.1826,293.6648,311.1270,329.6276,349.2282,369.9944,391.9954,415.3047,440,466.1638,493.8833};
 // C-0, C#-1, D-2, D#-3, E-4, F-5, F#-6, G-7, G#-8, A-9, A#-10, B-11
 
-#define Duration 350
-
+#define Duration 400 // do tre am thanh
+// khai bao chan nut
 const int LedPin[]={2,4,16,17,5,18,3,1,19,13,12,14};
 
 bool button0State = false;
@@ -50,22 +27,22 @@ bool button1State = false;
 bool button2State = false;
 bool button3State = false;
 bool button4State = false;
-// chan loa
+
 #define LED_COUNT 12
 
-
+// Hàm hướng dẫn chơi nhạc //
 void LedBlink( void *par ){
-  
+
   for (int repeat = 0; repeat < 3; repeat++) {
     // Led sáng lên từ bóng 1 đến bóng 12
     for (int i = 0; i < LED_COUNT; i++) {
       digitalWrite(LedPin[i], HIGH);
-      delay(83); // 1000ms / 12 LEDs = 83ms/LED
+      delay(43); // 1000ms / 12 LEDs = 83ms/LED
     }
-    // Led tắt từ bóng 12 đến bóng 1
-    for (int i = LED_COUNT - 1; i >= 0; i--) {
+    // Led tắt từ bóng 1 đến bóng 12
+    for (int i = 0; i < LED_COUNT; i++) {
       digitalWrite(LedPin[i], LOW);
-      delay(83); // 1000ms / 12 LEDs = 83ms/LED
+      delay(43); // 1000ms / 12 LEDs = 83ms/LED
     }
   }
 
@@ -82,98 +59,15 @@ void LedBlink( void *par ){
   for (int i = 0; i < SoluongNote ; i++)
   {
     digitalWrite(LedPin[music[i]],HIGH);
-    delay(450);
+    delay(400);
     digitalWrite(LedPin[music[i]],LOW);
   }
   
 }
 
-unsigned long lastPressedTime = 0;
-const unsigned long MAX_WAIT_TIME = 5000; // 5 giay
-
-// hàm để lưu nhạc
-void loadMelody_1 ( void *par ){
-  for (int i = 0; i < LED_COUNT; i++)
-  {
-    digitalWrite(LedPin[i],HIGH);
-  }
-  delay(1000);
-  for (int i = 0; i < LED_COUNT; i++)
-  {
-    digitalWrite(LedPin[i],LOW);
-  }
-  
-  bool recording = true;
-  lastPressedTime = millis();
-  do{
-    // kiểm tra thời gian
-  while (recording) {
-    // Kiểm tra xem đã quá thời gian chưa
-    if (millis() - lastPressedTime > MAX_WAIT_TIME) {
-      recording = false;
-    }
-  }
-  // Kiểm tra nút nào được bấm
-  for (int i = 0; i < 12; i++) {
-    if (digitalRead(BUTTON_PIN[i]) == HIGH) {
-      melody1[melodyIndex++] = i; // Lưu nút được bấm vào mảng nhạc
-      lastPressedTime = millis(); // Cập nhật thời gian lần cuối mà có sự kiện bấm nút
-    }
-  }
-  }while(recording == true);
-}
-
-// hàm để phát nhạc
-void playmelody(void *par ){
-  for (int i = 0; i < melodyIndex; i++) {
-    int note = melody1[i];
-    digitalWrite(LedPin[i],HIGH);
-    delay(450);
-    digitalWrite(LedPin[i],LOW);
-  }
-}
-
-
 void setup() {
 
   Serial.begin(115200);
-
-  
-  BLEDevice::init("PianoMini_TeamOne");
-
-  
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new MyServerCallbacks());
-
-  
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_NOTIFY
-                    );                   
-
-  
-  
-  pDescr = new BLEDescriptor((uint16_t)0x2901);
-  pDescr->setValue("A very interesting variable");
-  pCharacteristic->addDescriptor(pDescr);
-  
-  pBLE2902 = new BLE2902();
-  pBLE2902->setNotifications(true);
-  pCharacteristic->addDescriptor(pBLE2902);
-
-  
-  pService->start();
-
-  
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(false);
-  pAdvertising->setMinPreferred(0x0);  
-  BLEDevice::startAdvertising();
-  
 
   for (int i = 0; i < 12; i++)
   {
@@ -190,44 +84,18 @@ void setup() {
 
 void loop() {
 
-  
-    if (deviceConnected) {
-        pCharacteristic->setValue(value);
-        pCharacteristic->notify();
-        value++;
-        delay(1000);
-    }
-
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); 
-        pServer->startAdvertising(); // restart advertising
-
-        oldDeviceConnected = deviceConnected;
-    }
-
-    if (deviceConnected && !oldDeviceConnected) {
-
-        oldDeviceConnected = deviceConnected;
-    }
-
+  // đọc giá trị các button
   button0State = digitalRead(BUTTON_PIN[0]);
-  button11State = digitalRead(BUTTON_PIN[11]);
   button1State = digitalRead(BUTTON_PIN[1]);
-  button1State = digitalRead(BUTTON_PIN[2]);
 
-  if (button0State==true && button11State==true) {
+
+  // Nếu bấm phím 1 and 2 thì gọi hàm hướng dẫn ( core 2 )
+  if (button0State==true && button1State==true) {
     delay(2000);
     xTaskCreatePinnedToCore(LedBlink,"Task1",10000,NULL,1,&Task1,1);
   }
-  if (button0State==true && button1State==true) {
-    delay(2000);
-    xTaskCreatePinnedToCore(loadMelody_1,"Task1",10000,NULL,1,&Task1,1);
-  }
-  if (button0State==true && button2State==true) {
-    delay(2000);
-    xTaskCreatePinnedToCore(playmelody,"Task1",10000,NULL,1,&Task1,1);
-  }
   
+  // cài đặt các phím đàn ( core 1 )
 
   if (digitalRead(BUTTON_PIN[0]) == HIGH) {
     tone(BUZZER_PIN,NOTE[0]);
